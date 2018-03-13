2a7d2fb9b4e3db51b3bbdcbbb0f0c7eeac6cb1ec
Dakota Lambert <dakotalambert117@gmail.com>
2018-03-13 18:14:30 -0400

    Refactoring

--- Python Exercises/pets.py
+++ Python Exercises/pets.py
@@ -1,18 +1,7 @@
 import json
 
-# my_pets = [['Molly', 'dog', 'large', 'friendly'], ['Kitty', 'cat', 'medium', 'antisocial'], ['Purry', 'cat', 'small', 'sweet'], ['Togepi', 'hedghog', 'tiny', 'timid']]
-
-# my_pets = '{"pets":[{"name":"Molly","species":"dog","size":"large","personality":"friendly"},{"name":"Kitty","species":"cat","size":"medium","personality":"antisocial"},{"name":"Purry","species":"cat","size":"small","personality":"sweet"}]}'
-
 my_pets = '[{"name":"Molly","species":"dog","size":"large","personality":"friendly"},{"name":"Kitty","species":"cat","size":"medium","personality":"antisocial"},{"name":"Purry","species":"cat","size":"small","personality":"sweet"}]'
 pets = json.loads(my_pets)
 
 for pet in pets:
     print(pet["name"] + ' is a ' + pet["size"] + ', ' +  pet["personality"] + ' ' + pet["species"])
-
-
-# print('I have ' + str(len(pets)) + ' pets:')
-
-
-# for i in range(len(pets)) :
-#     print(pets[i][0] + ' is a ' + pets[i][2] + ', ' +  pets[i][3] + ' ' + pets[i][1])
