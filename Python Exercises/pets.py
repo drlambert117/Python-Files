70308e6b9db2910422e018ba351f4f8f09438e70
Dakota Lambert <dakotalambert117@gmail.com>
2018-03-13 18:13:56 -0400

    Added funtionality to display string from JSON array

--- Python Exercises/pets.py
+++ Python Exercises/pets.py
@@ -7,8 +7,8 @@ import json
 my_pets = '[{"name":"Molly","species":"dog","size":"large","personality":"friendly"},{"name":"Kitty","species":"cat","size":"medium","personality":"antisocial"},{"name":"Purry","species":"cat","size":"small","personality":"sweet"}]'
 pets = json.loads(my_pets)
 
-for i in pets:
-    print(i["name"])
+for pet in pets:
+    print(pet["name"] + ' is a ' + pet["size"] + ', ' +  pet["personality"] + ' ' + pet["species"])
 
 
 # print('I have ' + str(len(pets)) + ' pets:')
