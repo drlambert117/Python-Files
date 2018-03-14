203c26dc6e5ac5fb900702e74ee052df0bb35cf9
Dakota Lambert <dakotalambert117@gmail.com>
2018-03-14 18:47:13 -0400

    Added print statement for testing

--- Python Exercises/pets.py
+++ Python Exercises/pets.py
@@ -3,5 +3,7 @@ import json
 my_pets = '[{"name":"Molly","species":"dog","size":"large","personality":"friendly"},{"name":"Kitty","species":"cat","size":"medium","personality":"antisocial"},{"name":"Purry","species":"cat","size":"small","personality":"sweet"}]'
 pets = json.loads(my_pets)
 
+# print(my_pets)
+
 for pet in pets:
     print(pet["name"] + ' is a ' + pet["size"] + ', ' +  pet["personality"] + ' ' + pet["species"])
