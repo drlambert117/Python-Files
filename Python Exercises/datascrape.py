e3ab46814d420ab0bd3f99516aa02c26485b7ad3
Dakota Lambert <dakotalambert117@gmail.com>
2018-03-17 13:15:02 -0400

    Deleted user input capability

--- Python Exercises/datascrape.py
+++ Python Exercises/datascrape.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 site = 'https://en.wikipedia.org/wiki/List_of_state_and_union_territory_capitals_in_India'
-# site = input('Please paste the website url....')
+
 page = urllib.request.urlopen(site)
 
 soup = BeautifulSoup(page, "html.parser")
