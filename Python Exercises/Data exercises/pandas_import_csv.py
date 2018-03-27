1bdd39fd0efacc17993fe0906374ef0e2ef1faaa
Dakota Lambert <dakotalambert117@gmail.com>
2018-03-27 12:29:35 -0400

    Added Anaconda Installation to setting/ Prints 10 rows instead of 5

--- Python Exercises/Data exercises/pandas_import_csv.py
+++ Python Exercises/Data exercises/pandas_import_csv.py
@@ -3,6 +3,6 @@ import os
 
 CSV_PATH = os.path.join('data sets', 'artwork_data.csv')
 
-df = pd.read_csv(CSV_PATH, nrows=5, index_col='id')
+df = pd.read_csv(CSV_PATH, nrows=10, index_col='id')
 
 print(df)
