bc74e6b1e1d892244c7cc062e96b734de67967c7
Dakota Lambert <dakotalambert117@gmail.com>
2018-04-04 18:08:07 -0400

    Commenting

--- Python Exercises/Data exercises/pandas_import_csv.py
+++ Python Exercises/Data exercises/pandas_import_csv.py
@@ -2,16 +2,16 @@
 import pandas as pd
 import os
 
-#
+#Defines file path
 CSV_PATH = os.path.join('data sets', 'artwork_data.csv')
 
-#
+#Defines keys to pull from dataset
 COLS_TO_USE = ['artistId', 'artist', 'title', 'medium', 'year', 'acquisitionYear', 'height', 'width', 'units']
 
-#
+#re
 df = pd.read_csv(CSV_PATH, usecols=COLS_TO_USE)
 
 #Saves dataframe to pickle format in data sets folder
 # df.to_pickle(os.path.join('data sets', 'data_frame.pickle'))
 
-print(df) 
+# print(df) 
