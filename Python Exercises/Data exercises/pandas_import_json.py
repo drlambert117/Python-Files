ae4f58a65fd2a63586c377121f109058bf08dc2b
Dakota Lambert <dakotalambert117@gmail.com>
2018-04-04 11:42:52 -0400

    Commenting for class demo

--- Python Exercises/Data exercises/pandas_import_json.py
+++ Python Exercises/Data exercises/pandas_import_json.py
@@ -2,10 +2,13 @@ import pandas as pd
 import os
 import json
 
+"""Keys with useful data that we want pulled out of our dataset"""
 KEYS_TO_USE= ['id', 'all_artists', 'title', 'medium', 'dateText', 'acquisitionYear', 'height', 'width', 'units']
 
+"""==================================================================="""
+"""get_record() function: Processes single JSON file and returns a tuple containing specific fields"""
+"""==================================================================="""
 def get_record(file_path, keys_to_use):
-    """Processes single JSON file and returns a tuple containing specific fields"""
 
     with open(file_path) as artwork_file:
         content = json.load(artwork_file)
@@ -16,11 +19,17 @@ def get_record(file_path, keys_to_use):
 
     return tuple(record)
 
+"""Tests"""
 test_path = os.path.join('data sets', 'artworks', 'a', '000', 'a00001-1035.json')
-
 test_record= get_record(test_path, KEYS_TO_USE)
 
+
+"""========================================================================================"""
+"""read_artworks_from_json() function: Iterates through  JSON files in artworks folder and runs get_record() function on them"""
+"""========================================================================================"""
 def read_artworks_from_json(keys_to_use):
+
+    """Path to dataset"""
     JSON_ROOT = os.path.join('data sets', 'artworks')
 
     artworks = []
@@ -34,4 +43,5 @@ def read_artworks_from_json(keys_to_use):
     df = pd.DataFrame.from_records(artworks, columns=keys_to_use, index='id')
     return df
 
+"""Constructs the dataframe by funning the function and inputting the defined Keys"""
 df = read_artworks_from_json(KEYS_TO_USE)
