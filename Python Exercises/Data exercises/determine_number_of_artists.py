4145f5955830debceaf218eb33d8a882d33e37f9
Dakota Lambert <dakotalambert117@gmail.com>
2018-04-09 14:20:37 -0400

    Reads .pickle file and counts number of unique artists in the dataset

---
+++ Python Exercises/Data exercises/determine_number_of_artists.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import os
+
+df = pd.read_pickle(os.path.join('data sets', 'data_frame.pickle'))
+
+artists = df['artist']
+
+number_of_artists = len(pd.unique(artists))
+
+print(number_of_artists)
