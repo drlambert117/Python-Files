371a37c4aaeafc85bef0ddceb89013044fe17753
Dakota Lambert <dakotalambert117@gmail.com>
2018-04-09 15:20:00 -0400

    Refactoring

--- Python Exercises/Data exercises/dataset_filtering.py
+++ Python Exercises/Data exercises/dataset_filtering.py
@@ -5,9 +5,11 @@ df = pd.read_pickle(os.path.join('data sets', 'data_frame.pickle'))
 
 artists = df['artist']
 
-artist_name_to_filter = input('Please enter the name of a artist in "Last Name, First Name" format to see how many pieces they have created.')
+artist_first_name = input('Please enter the first name of the artist: ')
+artist_last_name = input('Please enter the last name of the artist: ')
+artist_name_to_filter = artist_last_name + ', ' + artist_first_name
 
 artist_counts = df['artist'].value_counts()
 number_of_works = artist_counts[artist_name_to_filter]
 
-print(artist_name_to_filter + ' has created ' + str(number_of_works) + ' works in the dataset')
+print(artist_first_name +  ' ' + artist_last_name + ' has created ' + str(number_of_works) + ' works in the dataset')
